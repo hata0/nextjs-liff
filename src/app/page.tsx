41ce84247aaf885b1fd16dde014729d8b99402b9
hata0 <haataa0100@gmail.com>
2024-08-14 22:47:41 +0900

    feat: liffから画像を取得して表示する機能を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { Profile } from "@liff/get-profile";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 import { useLiffContext } from "@/providers/LiffProvider";
@@ -21,11 +22,7 @@ export default function TopPage() {
     <div>
       {profile && (
         <div>
-          {/* <Image
-            src={profile.pictureUrl}
-            alt='profile'
-            className='rounded-full w-20 h-20 mx-auto mb-4'
-          /> */}
+          <Image alt="profile" height={80} src={profile.pictureUrl ?? ""} width={80} />
           <p className="text-center text-xl font-bold">userId: {profile.userId}</p>
           <p className="text-center text-gray-500">displayName: {profile.displayName}</p>
         </div>
