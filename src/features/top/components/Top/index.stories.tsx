8b5cc4d56594155fe5a69cfc0fa0a88956b2291b
hata0 <haataa0100@gmail.com>
2024-08-15 13:33:09 +0900

    feat: Meta と StoryObj の parameters を型付け

--- src/features/top/components/Top/index.stories.tsx
+++ src/features/top/components/Top/index.stories.tsx
@@ -1,9 +1,9 @@
-import { Meta, StoryObj } from "@storybook/react";
-
 import { Top } from ".";
 
 import { LIFF } from "@/tests/mocks/liff";
 import { PROFILE } from "@/tests/mocks/profile";
+import { Meta } from "@/tests/storybook/types/Meta";
+import { StoryObj } from "@/tests/storybook/types/StoryObj";
 
 type T = typeof Top;
 type Story = StoryObj<T>;
