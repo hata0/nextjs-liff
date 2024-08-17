5b9a0b65fa0581dd504e307476b4a7f7f0b4a903
hata0 <haataa0100@gmail.com>
2024-08-17 20:42:37 +0900

    feat: corsについて設定

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,27 @@ const nextConfig = {
   typescript: {
     tsconfigPath: process.env.NODE_ENV === "test" ? "tsconfig.test.json" : "tsconfig.json",
   },
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_BACKEND_URL ?? "",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,HEAD,PUT,PATCH,POST,DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
